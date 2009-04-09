b072c52a2d68eebf9e4854efd6734acc424a3cfb
David Vilar <vilar@i6.informatik.rwth-aachen.de>
2009-04-09 16:02:01 +0200

    Added zsh version. Renamed argc and argv to optArgc and optArgv
    zsh defines argv itself. Changed it also in bash version for consistency

--- options.zsh
+++ options.zsh
@@ -1,4 +1,10 @@
-# Options library for bash, v1.0
+# Options library for zsh, v1.0
+
+# Adapted from the bash version, probably some things could be done more
+# efficiently in zsh directly.
+# Main differences:
+# - Arrays start at 1 (zsh)
+# - dests variables have the $ included
 
 # Copyright 2009 David Vilar
 #
@@ -14,61 +20,63 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#function debug { $* > /dev/stderr; }
-function debug { true; }
+function debug { $* > /dev/stderr; }
+#function debug {}
 
 function setUsage() {
     __programUsage__="$*"
 }
 
-declare -a __shortOptions__
-declare -a __longOptions__
-declare -a __optionDests__
-declare -a __optionActions__
-declare -a __optionDefaults__
-declare -a __optionRequired__
-declare -a __optionFlag__
-declare -a __optionHelp__
+__shortOptions__=()
+#typeset -a __shortOptions__
+typeset -a __longOptions__
+typeset -a __optionDests__
+typeset -a __optionActions__
+typeset -a __optionDefaults__
+typeset -a __optionRequired__
+typeset -a __optionFlag__
+typeset -a __optionHelp__
 
-declare -a optArgv
+typeset -a optArgv
 optArgc=0
 __nOptions__=0;
 __programUsage__="usage: `basename $0` [options]\n\nOptions:\n"
 
 function addOption() {
+    ((__nOptions__++))
     debug echo "Option #${__nOptions__}:"
     # These arrays must have the same length
     __longOptions__[$__nOptions__]="___not_a_valid_option___"
     __shortOptions__[$__nOptions__]="___not_a_valid_option___"
     local i
     for i in "$@"; do
-        if [[ "${i:0:2}" = -- ]]; then
-            __longOptions__[$__nOptions__]=${i:2}
-            debug echo -e "\tRegistered long option ${i:2}"
+        if [[ "${i[1,2]}" = -- ]]; then
+            __longOptions__[$__nOptions__]=${i[3,-1]}
+            debug echo -e "\tRegistered long option ${i[3,-1]}"
 
-        elif [[ "${i:0:1}" = - ]]; then
-            __shortOptions__[$__nOptions__]=${i:1}
-            debug echo -e "\tRegistered short option ${i:1}"
+        elif [[ "${i[1]}" = - ]]; then
+            __shortOptions__[$__nOptions__]=${i[2,-1]}
+            debug echo -e "\tRegistered short option ${i[2,-1]}"
 
-        elif [[ "${i:0:5}" = dest= ]]; then
-            __optionDests__[$__nOptions__]=${i:5}
-            debug echo -e "\tOption dest is ${i:5}"
+        elif [[ "${i[1,5]}" = dest= ]]; then
+            __optionDests__[$__nOptions__]=\$${i[6,-1]}
+            debug echo -e "\tOption dest is ${i[6,-1]}"
 
-        elif [[ "${i:0:7}" = action= ]]; then
-            __optionActions__[$__nOptions__]=${i:7}
-            debug echo -e "\tOption action is ${i:7}"
+        elif [[ "${i[1,7]}" = action= ]]; then
+            __optionActions__[$__nOptions__]=${i[8,-1]}
+            debug echo -e "\tOption action is ${i[8,-1]}"
 
-        elif [[ "${i:0:8}" = default= ]]; then
-            __optionDefaults__[$__nOptions__]="${i:8}"
-            debug echo -e "\tOption default is ${i:8}"
+        elif [[ "${i[1,8]}" = default= ]]; then
+            __optionDefaults__[$__nOptions__]="${i[9,-1]}"
+            debug echo -e "\tOption default is ${i[9,-1]}"
 
         elif [[ "$i" = required ]]; then
             __optionRequired__[$__nOptions__]=1
             debug echo -e "\tOption required"
 
-        elif [[ "${i:0:5}" = help= ]]; then
-            __optionHelp__[$__nOptions__]=${i:5}
-            debug echo -e "\tRegistered help \"${i:5}\""
+        elif [[ "${i[1,5]}" = help= ]]; then
+            __optionHelp__[$__nOptions__]=${i[6,-1]}
+            debug echo -e "\tRegistered help \"${i[6,-1]}\""
 
         elif [[ "$i" = flagTrue ]]; then
             __optionFlag__[$__nOptions__]=1
@@ -83,14 +91,13 @@ function addOption() {
             exit 1
         fi
     done
-    ((__nOptions__++))
 }
 
 function __searchInArray__() {
     debug echo -e "\t__searchInArray__ $*"
     local searchFor=$1
     shift
-    local i=0;
+    local i=1;
     while [[ "$1" != "" ]]; do
         if [ $1 = $searchFor ]; then
             debug echo -e "\tFound as option $i"
@@ -104,12 +111,12 @@ function __searchInArray__() {
 
 function __searchOption__() {
     local pos
-    if [[ "${1:0:2}" = -- ]]; then
+    if [[ "${1[0,2]}" = -- ]]; then
         debug echo -e "\tIt's a long option"
-        pos=`__searchInArray__ ${1:2} ${__longOptions__[*]}`
-    elif [[ "${1:0:1}" = -  ]]; then
+        pos=`__searchInArray__ ${1[3,-1]} "${__longOptions__[@]}"`
+    elif [[ "${1[0,1]}" = - ]]; then
         debug echo -e "\tIt's a short option"
-        pos=`__searchInArray__ ${1:1} ${__shortOptions__[*]}`
+        pos=`__searchInArray__ ${1[2,-1]} ${__shortOptions__[*]}`
     fi
     debug echo -e "\tResulting pos: $pos"
     echo $pos
@@ -118,17 +125,17 @@ function __searchOption__() {
 
 function parseOptions() {
     # Set default values
-    local i=0;
-    while (($i < $__nOptions__)); do
+    local i=1;
+    while (($i <= $__nOptions__)); do
         default=${__optionDefaults__[$i]}
         if [[ "$default" != "" ]]; then
-            eval ${__optionDests__[$i]}=$default
+            eval ${__optionDests__[$i][2,-1]}=$default
         elif [[ "${__optionFlag__[$i]}" != "" ]]; then
             flag=${__optionFlag__[$i]}
             if [[ $flag = 1 ]]; then
-                eval ${__optionDests__[$i]}=false
+                eval ${__optionDests__[$i][2,-1]}=false
             else
-                eval ${__optionDests__[$i]}=true
+                eval ${__optionDests__[$i][2,-1]}=true
             fi
         fi
         ((i++))
@@ -144,17 +151,18 @@ function parseOptions() {
             else
                 local pos=`__searchOption__ "$1"`
                 if [[ "$pos" == "" ]]; then
-                    if [[ "${1:0:1}" = "-" ]]; then
+                    if [[ "${1[1]}" = "-" ]]; then
                         echo "Error: Unknown option $1" > /dev/stderr
                         exit 1
                     else
-                        optArgv[optArgc]="$1"
+                        debug echo "Argument found: $1"
                         ((optArgc++))
+                        optArgv[$optArgc]="$1"
                     fi
                 else
                     local dest=${__optionDests__[$pos]}
                     if [[ "$dest" != "" ]]; then
-                        debug echo "\tOption has a dest"
+                        debug echo "\tOption has a dest: $dest"
                         local isFlag=${__optionFlag__[$pos]}
                         if [[ "$isFlag" != "" ]]; then
                             debug echo -e "\tOption $i is a flag"
@@ -167,7 +175,7 @@ function parseOptions() {
                         else # It's not a flag
                             shift
                             value="$1"
-                            eval $dest=\"$value\"
+                            eval ${dest[2,-1]}=\"$value\"
                         fi
                     fi
 
@@ -179,19 +187,19 @@ function parseOptions() {
                 fi
             fi
         else # minusMinusSeen
-            optArgv[optArgc]="$1"
             ((optArgc++))
+            optArgv[$optArgc]="$1"
         fi
 
         shift
     done
 
     # Check for required values
-    i=0
+    i=1
     local -a missingOptions
-    while (($i < $__nOptions__)); do
-        debug echo "Checking option $i, required=\"${__optionRequired__[$i]}\", destValue=${!__optionDests__[$i]}"
-        if [[ "${__optionRequired__[$i]}" != "" && "${!__optionDests__[$i]}" = "" ]]; then
+    while (($i <= $__nOptions__)); do
+        debug echo "Checking option $i, required=\"${__optionRequired__[$i]}\", destValue=${(e)__optionDests__[$i]}"
+        if [[ "${__optionRequired__[$i]}" != "" && "${(e)__optionDests__[$i]}" = "" ]]; then
             debug echo -e "\tMissing option"
             local optionName=${__shortOptions__[$i]}
             if [[ "$optionName" = "___not_a_valid_option___" ]]; then
@@ -199,14 +207,14 @@ function parseOptions() {
             else
                 optionName="-"$optionName
             fi
-            missingOptions[${#missingOptions[*]}]=$optionName
+            missingOptions[${#missingOptions[*]}+1]=$optionName
         fi
         ((i++))
     done
     if (( ${#missingOptions[*]} > 0 )); then
-        echo -n "Error: following mandatory options are missing: ${missingOptions[0]}" > /dev/stderr
-        i=1
-        while (($i < ${#missingOptions[*]})); do
+        echo -n "Error: following mandatory options are missing: ${missingOptions[1]}" > /dev/stderr
+        i=2
+        while (($i <= ${#missingOptions[*]})); do
             echo -n ",${missingOptions[$i]}" > /dev/stderr
             ((i++))
         done
@@ -241,8 +249,8 @@ function __printOptionHelp__() {
 
 function __showHelp__() {
     echo -e "$__programUsage__"
-    local i=1 # -h is the first option
-    while (($i < $__nOptions__)); do
+    local i=2 # -h is the first option
+    while (($i <= $__nOptions__)); do
         __printOptionHelp__ $i
         ((++i))
     done
